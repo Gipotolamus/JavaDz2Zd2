3c5fa837d89b43bf03d8f31735139b260081ae7a
Mirau Pavel <miraupavel@gmail.com>
2023-08-13 13:42:29 +0700

    dorabotka 1

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int Balance = 100;
+        int balance = 100;
 
-        int Replenishment = 1231;
+        int replenishment = 1231;
 
-        boolean isReplenishment = true;
+//        boolean isReplenishment = true;
+//
+//        int ruble;
+//        if (isReplenishment) {
+//            ruble = 1;
+//        } else {
+//            ruble = 0;
+//        }
 
-        int ruble;
-        if (isReplenishment) {
-            ruble = 1;
-        } else {
-            ruble = 0;
-        }
-
-        int bonus = Replenishment / 100;
-        int check = Replenishment + Balance + bonus;
+        int bonus = replenishment / 100;
+        int check = replenishment + balance + bonus;
         System.out.println("Ваш баланс: " + check + " Рублей");
 
     }
